c41d7da8a848d8a076152555875f3a1aff42796e
AncizarTorres <ancizar_19_99.com>
2022-08-09 23:14:25 -0500

    ajuts name bands

---
+++ src/components/BandList.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+
+export const BandList = ({ data }) => {
+
+    const [bands, setBands] = useState([]);
+
+    useEffect(() => {
+        setBands(data)
+    }, [data])
+
+    const changeName = (e, id) => {
+        const newName = e.target.value;
+        setBands(bands => bands.map(band => {
+            if (band.id === id) {
+                band.name = newName;
+            }
+            return band;
+        }))
+    }
+
+    const onLostFocus = (id, name) => {
+        console.log(id, name);
+
+        // TODO:Disparar el evento Socket
+    }
+
+    const createRows = () => {
+        return (
+            bands.map(({ name, votes, id }) => (
+                <tr key={id}>
+                    <td>
+                        <button className='btn btn-primary'> +1 </button>
+                    </td>
+                    <td>
+                        <input
+                            className='form-control'
+                            value={name}
+                            onChange={(e) => changeName(e, id)}
+                            onBlur={() => onLostFocus(id, name)}
+                        />
+                    </td>
+                    <td><h3>{votes}</h3></td>
+                    <td>
+                        <button className='btn btn-danger'>Borrar</button>
+                    </td>
+                </tr>
+            ))
+        )
+    }
+
+    return (
+        <table className='table table-stripped'>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Nombre</th>
+                    <th>votos</th>
+                    <th>Borrar</th>
+                </tr>
+            </thead>
+            <tbody>
+                {createRows()}
+            </tbody>
+        </table>
+    )
+}
